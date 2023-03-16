401e545070065e58334b687c198ba797bf17ce1a
Anton <a.bredenbeck@tudelft.nl>
2023-03-16 17:32:36 +0100

    Clean up mocap publisher node

--- ws/src/am_vis/src/px4_topic_transformer.cpp
+++ ws/src/am_vis/src/px4_topic_transformer.cpp
@@ -30,18 +30,15 @@ class PX4TopicTransformer : public rclcpp::Node
     void pose_callback(const px4_msgs::msg::VehicleOdometry::SharedPtr msg)
     {
       auto message = geometry_msgs::msg::PoseStamped();
-      // TODO Double check with system start. Time stamps aren't synchronized
-      message.header.stamp = this->now(); //rclcpp::Time(msg->timestamp * 1000);
+      message.header.stamp = this->now(); 
       message.header.frame_id = "world";
+
 
       Eigen::Quaterniond orientation = px4_ros_com::frame_transforms::utils::quaternion::array_to_eigen_quat(msg->q);
       Eigen::Vector3d position = Eigen::Vector3d(msg->position[0], msg->position[1], msg->position[2]);
 
       orientation = px4_ros_com::frame_transforms::ned_to_enu_orientation(   
-                               px4_ros_com::frame_transforms::baselink_to_aircraft_orientation(
-                                                                                        orientation
-                                                                                              )
-                                                                        );
+                               px4_ros_com::frame_transforms::baselink_to_aircraft_orientation(orientation));
 
       position = px4_ros_com::frame_transforms::ned_to_enu_local_frame(position);                                                                  
                                                                     
@@ -56,6 +53,36 @@ class PX4TopicTransformer : public rclcpp::Node
 
       this->pose_publisher_->publish(message);
 
+    }
+
+    Eigen::Matrix3d _rot_x(double theta)
+    {
+      double sT = sin(theta), cT = cos(theta);
+      Eigen::Matrix3d rot;
+      rot << 1, 0, 0,
+            0, cT, -sT,
+            0, sT, cT;
+      return rot;
+    }
+
+    Eigen::Matrix3d _rot_y(double theta)
+    {
+
+      double sT = sin(theta), cT = cos(theta);
+      Eigen::Matrix3d rot;
+      rot << cT, 0, sT,
+            0, 1, 0,
+            -sT, 0, cT;
+      return rot;
+    }
+    Eigen::Matrix3d _rot_z(double theta)
+    {
+      double sT = sin(theta), cT = cos(theta);
+      Eigen::Matrix3d rot;
+      rot << cT, -sT, 0,
+            sT, cT, 0,
+            0, 0, 1;
+      return rot;
     }
 
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_publisher_;
