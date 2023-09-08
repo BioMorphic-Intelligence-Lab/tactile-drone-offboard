a81fa69d9ade990fe244957d70ec102decc37f8f
Anton Bredenbeck <a.bredenbeck@tudelft.nl>
2023-09-08 15:52:55 +0200

    Delete common folder

--- ws/src/common/include/common/constants.hpp
+++
@@ -1,7 +0,0 @@
-namespace common
-{
-    constexpr double SEC2USEC  = 1000000.0f;
-    constexpr double NSEC2USEC = 0.001;
-    constexpr double NSEC2SEC  = 1e-9f;
-    constexpr double SEC2NSEC  = 1e+9f;
-}
